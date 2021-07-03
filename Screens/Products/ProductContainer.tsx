c41406302c85c14014b5272e396ed2c9f6af097c
= <kevintuazon45@gmail.com>
2021-07-03 13:14:13 +0800

    added product list

--- Screens/Products/ProductContainer.tsx
+++ Screens/Products/ProductContainer.tsx
@@ -1,10 +1,59 @@
-import React, { useState } from "react";
-import { View, StyleSheet, Text, ActivityIndicator } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
+
+const data: IProduct[] = require("../../assets/data/products.json");
+
+interface IObjectId {
+  $oid: string;
+}
+
+interface IProduct {
+  _id: IObjectId;
+  image: string;
+  brand: string;
+  price: number;
+  rating: number;
+  numReviews: number;
+  isFeatured: boolean;
+  name: string;
+  description: string;
+  category: IObjectId;
+  countInStock: number;
+  __v: number;
+}
 
 const ProductContainer = () => {
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    setProducts(data);
+
+    return () => {
+      setProducts([]);
+    };
+  }, []);
+
+  const renderItem = ({ item }: { item: IProduct }) => (
+    <Text>{item.brand}</Text>
+  );
+
   return (
     <View style={styles.container}>
       <Text>Product Container</Text>
+      <View>
+        <FlatList
+          horizontal
+          data={products}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.name}
+        />
+      </View>
     </View>
   );
 };
